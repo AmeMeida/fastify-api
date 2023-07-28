5e840485a92ea86df1d95b77c5cb6e573047f30b
AmeMeida <ame.pistache@gmail.com>
2023-07-28 17:48:17 -0300

    exemplos de jsx

--- src/routes/html.tsx
+++ src/routes/html.tsx
@@ -1,5 +1,19 @@
 import { JSONSchema7 } from "json-schema";
 import { FastifyInstance } from "..";
+import { Attributes } from "typed-html";
+
+function HelloWorld({ children, ...props }: Attributes) {
+  return (
+    <html {...props}>
+      <head>
+        <title>Hello World</title>
+      </head>
+      <body>
+        <h1>{children}</h1>
+      </body>
+    </html>
+  );
+}
 
 export default async function (fastify: FastifyInstance) {
   fastify.get(
@@ -16,7 +30,29 @@ export default async function (fastify: FastifyInstance) {
       } as const
     },
     async (_request, reply) => {
-      reply.view(<b>Hello World</b>);
+      reply.view(
+        <HelloWorld>
+          <p>hi</p>
+        </HelloWorld>
+      );
+    }
+  );
+
+  fastify.get(
+    "/teste",
+    {
+      schema: {
+        response: {
+          200: {
+            type: "string",
+            contentMediaType: "text/html"
+          }
+        },
+        produces: ["text/html"]
+      }
+    },
+    (_request, reply) => {
+      reply.view(<h1>Teste</h1>);
     }
   );
 }
